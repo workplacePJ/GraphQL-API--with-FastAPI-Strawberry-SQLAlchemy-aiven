3ffe0db0cd4c256f6afaba091dbaa2526d1cb701
workplacePJ <workplace.pj@proton.me>
2024-11-16 09:21:56 +0900

    Update database.py

--- models/database.py
+++ models/database.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = config("POSTGRES_DB_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+DBSession = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
