0dd6d5d242ab38734f786f697f504aa96a6d55aa
workplacePJ <workplace.pj@proton.me>
2024-11-17 01:45:29 +0900

    Update database.py

--- models/database.py
+++ models/database.py
@@ -7,4 +7,20 @@ SQLALCHEMY_DATABASE_URL = config('POSTGRES_DB_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo = True)
 DBSession = sessionmaker(bind = engine, autoflush = False)
+Base = declarative_base() 
+
+
+
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://<username>:<password>@<host>/<database>"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
