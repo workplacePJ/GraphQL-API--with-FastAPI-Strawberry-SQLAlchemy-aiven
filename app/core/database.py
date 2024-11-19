937aa3963ba6a163d3777266a8a62e0f89f0d111
workplacePJ <workplace.pj@proton.me>
2024-11-19 16:12:10 +0900

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -4,9 +4,14 @@ from .config import settings
 
 DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 
-engine = create_async_engine(DATABASE_URL, echo=settings.DEBUG)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=settings.DEBUG
+)
 AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
 Base = declarative_base()
 
