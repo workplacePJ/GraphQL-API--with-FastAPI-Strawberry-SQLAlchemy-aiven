805bac63d6d2bfe2f0181d578028c02d8a7a187c
workplacePJ <workplace.pj@proton.me>
2024-11-19 15:28:40 +0900

    Update auth.py

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,1 +1,23 @@
+from pydantic import EmailStr
+from .base import BaseSchema
 
+class Token(BaseSchema):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class TokenPayload(BaseSchema):
+    sub: str | None = None
+    refresh: bool = False
+
+class UserAuth(BaseSchema):
+    email: EmailStr
+    password: str
+
+class UserCreate(UserAuth):
+    full_name: str
+
+class User(BaseSchema):
+    email: EmailStr
+    full_name: str
+    is_active: bool = True
