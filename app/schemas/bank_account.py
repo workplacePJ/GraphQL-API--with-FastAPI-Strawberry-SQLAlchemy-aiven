482d7903d1dc67ed38b80492da33cdef4e9a2c5c
workplacePJ <workplace.pj@proton.me>
2024-11-19 15:26:23 +0900

    Update bank_account.py

--- app/schemas/bank_account.py
+++ app/schemas/bank_account.py
@@ -1,1 +1,26 @@
+from pydantic import Field
+from .base import BaseSchema, IDSchema
+from uuid import UUID
+from app.models.enums import AccountType
 
+class BankAccountBase(BaseSchema):
+    bank_name: str
+    branch_name: str
+    account_type: AccountType
+    account_number: str
+    account_holder: str
+
+class BankAccountCreate(BankAccountBase):
+    holder_id: UUID
+
+class BankAccountUpdate(BankAccountBase):
+    bank_name: str | None = None
+    branch_name: str | None = None
+    account_type: AccountType | None = None
+    account_number: str | None = None
+    account_holder: str | None = None
+
+class BankAccountInDB(BankAccountBase, IDSchema):
+    holder_id: UUID
+
+BankAccount = BankAccountInDB
